3d6584426917b497d2db821b337dba4d19cdc69d
alvin <953909858@qq.com>
2019-11-06 12:26:20 +0800

    管理员角色model

---
+++ app/Models/Admin/BackendAdminAccessGroup.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models\Admin;
+
+use App\Models\BaseModel;
+
+/**
+ * Class for backend admin access group.
+ */
+class BackendAdminAccessGroup extends BaseModel
+{
+    /**
+     * @var array
+     */
+    protected $guarded = ['id'];
+    
+    /**
+     * Gets the table columns.
+     *
+     * @return array
+     */
+    public function getTableColumns()
+    {
+        return $this
+            ->getConnection()
+            ->getSchemaBuilder()
+            ->getColumnListing($this->getTable());
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function adminUsers()
+    {
+        return $this->hasMany(BackendAdminUser::class, 'group_id', 'id')
+            ->select(['id', 'name', 'email', 'is_test', 'status', 'platform_id', 'group_id']);
+    }
+}
