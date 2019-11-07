86e64d955be6aa18de4ed578703f9725f2a834ba
alvin <953909858@qq.com>
2019-11-07 10:14:36 +0800

    总后台-添加菜单&编辑菜单  Action

---
+++ app/Http/Controllers/SingleActions/Backend/Headquarters/DeveloperUsage/Backend/Menu/MenuEditAction.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\SingleActions\Backend\Headquarters\DeveloperUsage\Backend\Menu;
+
+use App\Models\DeveloperUsage\Menu\BackendSystemMenu;
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Class for menu edit action.
+ */
+class MenuEditAction
+{
+    /**
+     * @var BackendSystemMenu
+     */
+    protected $model;
+
+    /**
+     * @param BackendSystemMenu $backendSystemMenu Model.
+     */
+    public function __construct(BackendSystemMenu $backendSystemMenu)
+    {
+        $this->model = $backendSystemMenu;
+    }
+
+    /**
+     * @param array $inputDatas 传递的参数.
+     * @return JsonResponse
+     */
+    public function execute(array $inputDatas): JsonResponse
+    {
+        $parent = false;
+        if (isset($inputDatas['is_parent']) && (int) $inputDatas['is_parent'] === 1) {
+            $parent = true;
+        }
+        $menuEloq = $this->model::find($inputDatas['menu_id']);
+        if ($menuEloq) {
+            $menuEloq->label = $inputDatas['label'];
+            $menuEloq->en_name = $inputDatas['en_name'];
+            $menuEloq->display = $inputDatas['display'];
+            $menuEloq->icon = $inputDatas['icon'] ?? null;
+            if ($parent === true) {
+                $menuEloq->route = '#';
+                $menuEloq->pid = 0;
+            } else {
+                $menuEloq->route = $inputDatas['route'];
+                $menuEloq->pid = $inputDatas['parent_id'];
+            }
+            $data = $menuEloq->toArray();
+            if ($menuEloq->save()) {
+                return msgOut(true, $data);
+            } else {
+                return msgOut(false, [], '100801');
+            }
+        } else {
+            return msgOut(false);
+        }
+    }
+}
