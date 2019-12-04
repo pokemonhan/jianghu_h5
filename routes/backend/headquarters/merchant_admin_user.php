4e2dbbdeae0f665c8e903c5f81a1bebead552051
alvin <953909858@qq.com>
2019-12-04 16:32:31 +0800

    t

--- routes/backend/headquarters/merchant_admin_user.php
+++ routes/backend/headquarters/merchant_admin_user.php
@@ -1,13 +1,16 @@
 <?php
 
 //运营商管理
-Route::group(['prefix' => 'platform', 'namespace' => 'Merchant'], static function () {
-    $namePrefix = 'headquarters-api.platform.';
-    $controller = 'PlatformController@';
-    //运营商列表
-    Route::match(['get', 'options'], 'detail', ['as' => $namePrefix . 'detail', 'uses' => $controller . 'detail']);
-    //添加运营商
-    Route::match(['post', 'options'], 'do-add', ['as' => $namePrefix . 'do-add', 'uses' => $controller . 'doAdd']);
-    //运营商状态开关
-    Route::match(['post', 'options'], 'switch', ['as' => $namePrefix . 'switch', 'uses' => $controller . 'switch']);
-});
+Route::group(
+	['prefix' => 'platform', 'namespace' => 'Merchant'],
+	static function () {
+	    $namePrefix = 'headquarters-api.platform.';
+	    $controller = 'PlatformController@';
+	    //运营商列表
+	    Route::match(['get', 'options'], 'detail', ['as' => $namePrefix . 'detail', 'uses' => $controller . 'detail']);
+	    //添加运营商
+	    Route::match(['post', 'options'], 'do-add', ['as' => $namePrefix . 'do-add', 'uses' => $controller . 'doAdd']);
+	    //运营商状态开关
+	    Route::match(['post', 'options'], 'switch', ['as' => $namePrefix . 'switch', 'uses' => $controller . 'switch']);
+	}
+);
