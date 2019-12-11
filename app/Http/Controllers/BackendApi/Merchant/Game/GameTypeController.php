304e40a471f342a19c089a71b75d3ca302c76fec
tab <ckx9667131121@gmail.com>
2019-12-11 18:16:02 +0800

    规范

--- app/Http/Controllers/BackendApi/Merchant/Game/GameTypeController.php
+++ app/Http/Controllers/BackendApi/Merchant/Game/GameTypeController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\BackendApi\Merchant\Game;
 
 use App\Http\Controllers\BackendApi\Merchant\MerchantApiMainController;
 
+/**
+ * Class GameTypeController
+ * @package App\Http\Controllers\BackendApi\Merchant\Game
+ */
 class GameTypeController extends MerchantApiMainController
 {
-    //
+
 }
