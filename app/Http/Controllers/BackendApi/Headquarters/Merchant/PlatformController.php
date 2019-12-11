c23dba595a278031a29a9096f3d42a9433de4358
tab <ckx9667131121@gmail.com>
2019-12-11 15:03:38 +0800

    修改platformcontroller

--- app/Http/Controllers/BackendApi/Headquarters/Merchant/PlatformController.php
+++ app/Http/Controllers/BackendApi/Headquarters/Merchant/PlatformController.php
@@ -113,7 +113,7 @@ class PlatformController extends BackEndApiMainController
     public function assignedGames(AssignedGamesAction $action, AssignedGamesRequest $request):JsonResponse
     {
         $inputDatas = $request->validated();
-        return $action->execute($this, $inputDatas);
+        return $action->execute($inputDatas);
     }
 
     /**
@@ -126,7 +126,7 @@ class PlatformController extends BackEndApiMainController
     public function unassignGames(UnassignGamesAction $action, UnassignGamesRequest $request) :JsonResponse
     {
         $inputDatas = $request->validated();
-        return $action->execute($this, $inputDatas);
+        return $action->execute($inputDatas);
     }
 
     /**
