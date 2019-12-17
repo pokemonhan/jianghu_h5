49270f6105e3321439cb2323673ac38f0ca23399
tab <ckx9667131121@gmail.com>
2019-12-17 17:39:37 +0800

    t

--- app/Http/SingleActions/Backend/Merchant/Game/StatusAction.php
+++ app/Http/SingleActions/Backend/Merchant/Game/StatusAction.php
@@ -1,21 +1,26 @@
 <?php
+
 namespace App\Http\SingleActions\Backend\Merchant\Game;
 
 use Illuminate\Http\JsonResponse;
 
+/**
+ * Class StatusAction
+ * @package App\Http\SingleActions\Backend\Merchant\Game
+ */
 class StatusAction extends BaseAction
 {
 
-   /**
-    * @param array $inputDatas InputDatas.
-    * @return JsonResponse
-    * @throws \Exception Exception.
-    */
-   public function execute(array $inputDatas) :JsonResponse
-   {
-       if ($this->model->where('id', $inputDatas['id'])->update(['status' => $inputDatas['status']])) {
-           return msgOut(true);
-       }
-       throw new \Exception('300203');
-   }
+    /**
+     * @param array $inputDatas InputDatas.
+     * @return JsonResponse
+     * @throws \Exception Exception.
+     */
+    public function execute(array $inputDatas) :JsonResponse
+    {
+        if ($this->model->where('id', $inputDatas['id'])->update(['status' => $inputDatas['status']])) {
+            return msgOut(true);
+        }
+        throw new \Exception('300203');
+    }
 }
