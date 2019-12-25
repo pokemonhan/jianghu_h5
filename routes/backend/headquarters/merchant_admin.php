788cdfeb4f71bea244b2f2d41e3a244c2c4439f6
alvin <953909858@qq.com>
2019-12-25 13:39:04 +0800

    路由换成新的方式

--- routes/backend/headquarters/merchant_admin.php
+++ routes/backend/headquarters/merchant_admin.php
@@ -1,42 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * author: Harris
- * Date: 4/11/2019
- * Time: 12:22 PM
- */
 
 use App\Http\Controllers\BackendApi\Merchant\Admin\MerchantAdminUserController;
 
 //管理员相关
 Route::group(
     ['prefix' => 'merchant-admin-user', 'namespace' => 'Admin'],
-    static function () {
+    static function (): void {
         $namePrefix = 'merchant-api.merchant-admin-user.';
         Route::match(
             ['post', 'options'],
             'create',
             [MerchantAdminUserController::class, 'create'],
-        );
+        )->name($namePrefix . 'create');
         Route::match(
             ['get', 'options'],
             'get-all-admins',
             [MerchantAdminUserController::class, 'allAdmins'],
-        );
+        )->name($namePrefix . 'get-all-admins');
         Route::match(
             ['post', 'options'],
             'update-admin-group',
             [MerchantAdminUserController::class, 'updateAdminGroup'],
-        );
+        )->name($namePrefix . 'update-admin-group');
         Route::match(
             ['post', 'options'],
             'delete-admin',
             [MerchantAdminUserController::class, 'deletePartnerAdmin'],
-        );
+        )->name($namePrefix . 'delete-admin');
         Route::match(
             ['post', 'options'],
             'search-admin',
             [MerchantAdminUserController::class, 'searchAdmin'],
-        );
+        )->name($namePrefix . 'search-admin');
     },
 );
