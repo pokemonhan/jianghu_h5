465fcb48d955cdaa6c0bd13bffe31ddbab561cce
tab <ckx9667131121@gmail.com>
2019-12-30 21:01:29 +0800

    将文件的存放路径移动到storage/statics目录中

--- config/upload.php
+++ config/upload.php
@@ -4,5 +4,5 @@ return [
     'input_name' => 'file', //表单键的 name
     'max_size' => '1000000', //最大文件大小 单位 kb
     'mimes' => 'image/jpeg,image/png,image/bmp,image/gif', //可以接受的文件类型
-    'disk' => 'public', //储存路径 对应 filesystems 中的磁盘配置
+    'disk' => 'statics', //储存路径 对应 filesystems 中的磁盘配置
 ];
