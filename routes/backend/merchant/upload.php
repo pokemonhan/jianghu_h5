2924f75bb616f26af8ff2c445ea6894a201077fa
ethan <ethan.kekeke@gmail.com>
2020-01-01 18:47:43 +0800

    update merchant upload

--- routes/backend/merchant/upload.php
+++ routes/backend/merchant/upload.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CommonApi\UploadController;
 
 //上传图片
 Route::group(
-    ['prefix' => 'merchant'],
+    [],
     static function (): void {
         $namePrefix = 'merchant-api.';
         Route::post(
