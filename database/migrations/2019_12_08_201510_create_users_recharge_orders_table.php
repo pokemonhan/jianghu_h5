38cd02e61c4a308669630cfa71db1b52ceab4555
ethan <ethan.kekeke@gmail.com>
2020-01-10 13:27:43 +0800

    add some fields

--- database/migrations/2019_12_08_201510_create_users_recharge_orders_table.php
+++ database/migrations/2019_12_08_201510_create_users_recharge_orders_table.php
@@ -23,21 +23,30 @@ class CreateUsersRechargeOrdersTable extends Migration
                 $table->increments('id');
                 $table->collation = 'utf8mb4_0900_ai_ci';
                 $table->string('platform_sign', 32)->nullable()->default(null)->comment('平台标识');
+                $table->string('platform_no', 128)->nullable()->default(null)->comment('第三方订单号');
+                $table->string('platform_need_no', 128)->nullable()->default(null)->comment('第三方需要的订单号');
                 $table->string('order_no', 128)->nullable()->default(null)->comment('订单号');
                 $table->integer('user_id')->nullable()->default(null)->comment('用户ID');
-                $table->string('finance_type_sign', 64)->nullable()->default(null)->comment('金流类型标识');
+                $table->integer('finance_type_id')->default(0)->comment('金流类型id');
                 $table->integer('finance_channel_id')->nullable()->default(null)->comment('充值渠道ID');
                 $table->decimal('money', 18, 4)->nullable()->default(null)->comment('订单金额');
                 $table->decimal('real_money', 18, 4)->nullable()->default(null)->comment('真实付款金额');
                 $table->decimal('discount_money', 18, 4)->nullable()->default(null)->comment('优惠金额');
                 $table->decimal('handling_money', 18, 4)->nullable()->default(null)->comment('手续费');
-                $table->decimal('total_money', 18, 4)->nullable()->default(null)->comment('总金额');
+                $table->decimal('arrive_money', 18, 4)->nullable()->default(null)->comment('到帐金额');
                 $table->tinyInteger('status')->nullable()->default(null)->comment('状态 0 审核中 1 审核通过');
                 $table->integer('admin_id')->nullable()->default(null)->comment('操作人ID');
                 $table->tinyInteger('is_online')->nullable()->default(null)->comment('是否是线上存款 1 是 0 否');
                 $table->tinyInteger('recharge_status')->nullable()->default(null)->comment('支付状态 1 已支付 0 未支付');
                 $table->string('remark')->nullable()->default(null)->comment('备注');
                 $table->string('client_ip', 30)->default(null)->comment('下单ip');
+                $table->string('snap_merchant_no', 128)->default(null)->comment('商户编号快照');
+                $table->string('snap_merchant_code')->default(null)->comment('商户号快照');
+                $table->string('snap_merchant', 64)->default(null)->comment('商户快照');
+                $table->string('snap_finance_type', 64)->default(null)->comment('支付方式快照');
+                $table->string('snap_user_grade', 64)->default(null)->comment('会员等级快照');
+                $table->string('snap_account', 128)->default(null)->comment('收款账户快照');
+                $table->string('snap_bank', 64)->default(null)->comment('收款银行快照');
 
                 $table->nullableTimestamps();
             },
