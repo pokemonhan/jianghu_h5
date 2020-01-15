e6fb6ac3bc572144788492be560b8aba2e64e9a2
Harris <harrisdt15f@gmail.com>
2020-01-15 13:02:25 +0800

    优化代码规范 文件: help_center.php

--- routes/backend/merchant/help_center.php
+++ routes/backend/merchant/help_center.php
@@ -10,22 +10,34 @@ Route::group(
         //列表
         Route::post(
             'index',
-            [HelpCenterController::class, 'index'],
+            [
+             HelpCenterController::class,
+             'index',
+            ],
         )->name($namePrefix . 'index');
         //添加
         Route::post(
             'do-add',
-            [HelpCenterController::class, 'doAdd'],
+            [
+             HelpCenterController::class,
+             'doAdd',
+            ],
         )->name($namePrefix . 'do-add');
         //编辑
         Route::post(
             'edit',
-            [HelpCenterController::class, 'edit'],
+            [
+             HelpCenterController::class,
+             'edit',
+            ],
         )->name($namePrefix . 'edit');
         //删除
         Route::post(
             'delete',
-            [HelpCenterController::class, 'delete'],
+            [
+             HelpCenterController::class,
+             'delete',
+            ],
         )->name($namePrefix . 'delete');
     },
 );
