6289e354835744359b10f69269aecf5bea30daf6
Harris <harrisdt15f@gmail.com>
2020-01-15 12:47:47 +0800

    优化代码规范 文件: StatusRequest.php

--- app/Http/Requests/Backend/Headquarters/Setting/SmsConfig/StatusRequest.php
+++ app/Http/Requests/Backend/Headquarters/Setting/SmsConfig/StatusRequest.php
@@ -27,9 +27,9 @@ class StatusRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            'id'     => 'required|exists:system_sms_configs', //ID
-            'status' => 'required|integer|in:0,1',            //状态 0禁用 1启用
-        ];
+                'id'     => 'required|exists:system_sms_configs', //ID
+                'status' => 'required|integer|in:0,1',            //状态 0禁用 1启用
+               ];
     }
 
     /**
@@ -38,11 +38,11 @@ class StatusRequest extends BaseFormRequest
     public function messages(): array
     {
         return [
-            'id.required'     => '缺少短信配置ID',
-            'id.exists'       => '该短信配置不存在',
-            'status.required' => '缺少短信配置状态',
-            'status.integer'  => '短信配置状态必须是整数',
-            'status.in'       => '短信配置状态数据非法',
-        ];
+                'id.required'     => '缺少短信配置ID',
+                'id.exists'       => '该短信配置不存在',
+                'status.required' => '缺少短信配置状态',
+                'status.integer'  => '短信配置状态必须是整数',
+                'status.in'       => '短信配置状态数据非法',
+               ];
     }
 }
