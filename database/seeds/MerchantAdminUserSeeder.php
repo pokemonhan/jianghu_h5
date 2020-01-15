020baa7d2b246f33ff0e5d9cea77c310eb77efe0
Harris <harrisdt15f@gmail.com>
2020-01-15 12:59:28 +0800

    优化代码规范 文件: MerchantAdminUserSeeder.php

--- database/seeds/MerchantAdminUserSeeder.php
+++ database/seeds/MerchantAdminUserSeeder.php
@@ -17,18 +17,18 @@ class MerchantAdminUserSeeder extends Seeder
     {
         MerchantAdminUser::insert(
             [
-                [
-                    'id' => '1',
-                    'name' => 'ling',
-                    'email' => 'ling@gmail.com',
-                    'password' => '$2y$10$MKKGeBpoDfzfcewJ4Mrk.uTCIcSSvowXwPeKw0LNwY8K60pz7QL3G',
-                    'remember_token' => '',
-                    'is_test' => '0',
-                    'group_id' => '1',
-                    'status' => '1',
-                    'platform_sign' => 'test',
-                    'chargeable_fund' => 100,
-                ],
+             [
+              'id'              => '1',
+              'name'            => 'ling',
+              'email'           => 'ling@gmail.com',
+              'password'        => '$2y$10$MKKGeBpoDfzfcewJ4Mrk.uTCIcSSvowXwPeKw0LNwY8K60pz7QL3G',
+              'remember_token'  => '',
+              'is_test'         => '0',
+              'group_id'        => '1',
+              'status'          => '1',
+              'platform_sign'   => 'test',
+              'chargeable_fund' => 100,
+             ],
             ],
         );
     }
