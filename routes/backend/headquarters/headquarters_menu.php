d08b37234b6ac20de88969350d75473e383af56b
Harris <harrisdt15f@gmail.com>
2020-01-15 13:01:15 +0800

    优化代码规范 文件: headquarters_menu.php

--- routes/backend/headquarters/headquarters_menu.php
+++ routes/backend/headquarters/headquarters_menu.php
@@ -17,33 +17,54 @@ Route::group(
         //获取商户用户的所有菜单
         Route::get(
             'get-all-menu',
-            [MenuController::class, 'getAllMenu'],
+            [
+             MenuController::class,
+             'getAllMenu',
+            ],
         )->name($namePrefix . 'get-all-menu');
         //获取当前商户用户的菜单
         Route::get(
             'current-admin-menu',
-            [MenuController::class, 'currentAdminMenu'],
+            [
+             MenuController::class,
+             'currentAdminMenu',
+            ],
         )->name($namePrefix . 'current-admin-menu');
         //所有菜单需要参数 【父级，路由名，可编辑菜单信息】
         Route::post(
             '/',
-            [MenuController::class, 'allRequireInfos'],
+            [
+             MenuController::class,
+             'allRequireInfos',
+            ],
         )->name($namePrefix . 'all-require-infos');
         Route::post(
             '/change-parent',
-            [MenuController::class, 'changeParent'],
+            [
+             MenuController::class,
+             'changeParent',
+            ],
         )->name($namePrefix . 'change-parent');
         Route::post(
             '/add',
-            [MenuController::class, 'doAdd'],
+            [
+             MenuController::class,
+             'doAdd',
+            ],
         )->name($namePrefix . 'add');
         Route::post(
             '/edit',
-            [MenuController::class, 'edit'],
+            [
+             MenuController::class,
+             'edit',
+            ],
         )->name($namePrefix . 'edit');
         Route::post(
             '/delete',
-            [MenuController::class, 'delete'],
+            [
+             MenuController::class,
+             'delete',
+            ],
         )->name($namePrefix . 'delete');
     },
 );
