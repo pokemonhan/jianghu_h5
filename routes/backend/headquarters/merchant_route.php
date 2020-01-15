c74478f2de99cb43f052f9442d57232be0fe9fe0
Harris <harrisdt15f@gmail.com>
2020-01-15 13:01:29 +0800

    优化代码规范 文件: merchant_route.php

--- routes/backend/headquarters/merchant_route.php
+++ routes/backend/headquarters/merchant_route.php
@@ -10,22 +10,34 @@ Route::group(
         //路由-列表
         Route::get(
             'index',
-            [RouteController::class, 'index'],
+            [
+             RouteController::class,
+             'index',
+            ],
         )->name($namePrefix . 'index');
         //路由-添加
         Route::post(
             'do-add',
-            [RouteController::class, 'doAdd'],
+            [
+             RouteController::class,
+             'doAdd',
+            ],
         )->name($namePrefix . 'do-add');
         //路由-编辑
         Route::post(
             'edit',
-            [RouteController::class, 'edit'],
+            [
+             RouteController::class,
+             'edit',
+            ],
         )->name($namePrefix . 'edit');
         //路由-删除
         Route::post(
             'delete',
-            [RouteController::class,  'delete'],
+            [
+             RouteController::class,
+             'delete',
+            ],
         )->name($namePrefix . 'delete');
     },
 );
