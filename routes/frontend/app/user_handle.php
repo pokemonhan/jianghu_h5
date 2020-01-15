2d4b83e575ff7c60696daecbde5b9ef420a934e1
Harris <harrisdt15f@gmail.com>
2020-01-15 13:03:13 +0800

    优化代码规范 文件: user_handle.php

--- routes/frontend/app/user_handle.php
+++ routes/frontend/app/user_handle.php
@@ -4,18 +4,18 @@ use App\Http\Controllers\FrontendApi\Common\FrontendAuthController;
 use App\Http\Controllers\FrontendApi\Common\FrontendUserController;
 use App\Http\Controllers\FrontendApi\Common\PasswordController;
 
-Route::post('login', [FrontendAuthController::class,'login'])->name('app-api.login');
+Route::post('login', [FrontendAuthController::class, 'login'])->name('app-api.login');
 
 //管理总代用户与玩家
 Route::group(
     ['prefix' => 'user'],
     static function (): void {
         $namePrefix = 'app-api.user.';
-        Route::get('logout', [FrontendAuthController::class,'logout'])->name($namePrefix . 'logout');
-        Route::put('reset-password', [PasswordController::class,'password'])->name($namePrefix . 'reset-password');
-        Route::put('refresh-token', [FrontendAuthController::class,'refreshToken'])
+        Route::get('logout', [FrontendAuthController::class, 'logout'])->name($namePrefix . 'logout');
+        Route::put('reset-password', [PasswordController::class, 'password'])->name($namePrefix . 'reset-password');
+        Route::put('refresh-token', [FrontendAuthController::class, 'refreshToken'])
             ->name($namePrefix . 'refresh-token');
-        Route::get('information', [FrontendUserController::class,'information'])->name($namePrefix . 'information');
-        Route::get('grades', [FrontendUserController::class,'grades'])->name($namePrefix . 'grades');
+        Route::get('information', [FrontendUserController::class, 'information'])->name($namePrefix . 'information');
+        Route::get('grades', [FrontendUserController::class, 'grades'])->name($namePrefix . 'grades');
     },
 );
