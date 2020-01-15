980b5ff760e33f2b3513abacb5969880ec5ee7c5
Harris <harrisdt15f@gmail.com>
2020-01-15 12:51:33 +0800

    优化代码规范 文件: IndexResource.php

--- app/Http/Resources/Backend/Headquarters/Setting/SmsConfig/IndexResource.php
+++ app/Http/Resources/Backend/Headquarters/Setting/SmsConfig/IndexResource.php
@@ -19,20 +19,20 @@ class IndexResource extends JsonResource
     public function toArray($request): array
     {
         $result = [
-            'id'                 => $this->id,
-            'name'               => $this->name,
-            'sign'               => $this->sign,
-            'merchant_code'      => $this->merchant_code,
-            'merchant_secret'    => $this->merchant_secret,
-            'public_key'         => $this->public_key,
-            'authorization_code' => $this->authorization_code,
-            'sms_num'            => $this->sms_num,
-            'url'                => $this->url,
-            'status'             => $this->status,
-            'last_editor_name'   => $this->admin->name,
-            'updated_at'         => $this->updated_at,
-            'created_at'         => $this->created_at,
-        ];
+                   'id'                 => $this->id,
+                   'name'               => $this->name,
+                   'sign'               => $this->sign,
+                   'merchant_code'      => $this->merchant_code,
+                   'merchant_secret'    => $this->merchant_secret,
+                   'public_key'         => $this->public_key,
+                   'authorization_code' => $this->authorization_code,
+                   'sms_num'            => $this->sms_num,
+                   'url'                => $this->url,
+                   'status'             => $this->status,
+                   'last_editor_name'   => $this->admin->name,
+                   'updated_at'         => $this->updated_at,
+                   'created_at'         => $this->created_at,
+                  ];
         return $result;
     }
 }
