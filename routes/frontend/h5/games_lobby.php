cf760f04ce55ae6f688d3237e917b52a376cfa09
Harris <harrisdt15f@gmail.com>
2020-01-15 13:03:17 +0800

    优化代码规范 文件: games_lobby.php

--- routes/frontend/h5/games_lobby.php
+++ routes/frontend/h5/games_lobby.php
@@ -6,9 +6,9 @@ Route::group(
     ['prefix' => 'games-lobby'],
     static function (): void {
         $namePrefix = 'h5-api.games-lobby.';
-        Route::get('rich-list', [GamesLobbyController::class,'richList'])->name($namePrefix . 'rich-list');
-        Route::post('game-categories', [GamesLobbyController::class,'category'])->name($namePrefix . 'category');
-        Route::post('game-list', [GamesLobbyController::class,'gameList'])->name($namePrefix . 'game-list');
-        Route::post('slides', [GamesLobbyController::class,'slides'])->name($namePrefix . 'slides');
+        Route::get('rich-list', [GamesLobbyController::class, 'richList'])->name($namePrefix . 'rich-list');
+        Route::post('game-categories', [GamesLobbyController::class, 'category'])->name($namePrefix . 'category');
+        Route::post('game-list', [GamesLobbyController::class, 'gameList'])->name($namePrefix . 'game-list');
+        Route::post('slides', [GamesLobbyController::class, 'slides'])->name($namePrefix . 'slides');
     },
 );
