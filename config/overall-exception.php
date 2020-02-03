d1ee231d678e3332b21b5c276d4d797e5c053c3b
Harris <harrisdt15f@gmail.com>
2020-02-03 14:08:27 +0800

    :sparkles: 性能提升

--- config/overall-exception.php
+++ config/overall-exception.php
@@ -4,29 +4,33 @@ use App\Lib\ErrorsHandler\Formatters;
 use App\Lib\ErrorsHandler\ResponseFactory;
 use Symfony\Component\HttpKernel\Exception as SymfonyException;
 
-return [
-    'add_cors_headers' => false,
+// phpcs:disable Generic.Files.LineLength.MaxExceeded,Generic.Files.LineLength.TooLong
+
+$config = [
+           'add_cors_headers'        => false,
     // Has to be in prioritized order, e.g. highest priority first.
-    'formatters' => [
-        SymfonyException\UnprocessableEntityHttpException::class
-        => Formatters\UnprocessableEntityHttpExceptionFormatter::class,
-        SymfonyException\HttpException::class => Formatters\HttpExceptionFormatter::class,
-        \Illuminate\Validation\ValidationException::class  => Formatters\ValidationExceptionFormatter::class,
-        PDOException::class => Formatters\PDOExceptionFormatter::class,
-        \Illuminate\Auth\AuthenticationException::class => Formatters\AuthExceptionFormatter::class,
-        Exception::class => Formatters\ExceptionFormatter::class,
-    ],
-    'response_factory' => ResponseFactory::class,
-    'reporters' => [
-        /*'sentry' => [
-            'class'  => \Optimus\Heimdal\Reporters\SentryReporter::class,
-            'config' => [
-                'dsn' => '',
-                // For extra options see https://docs.sentry.io/clients/php/config/
-                // php version and environment are automatically added.
-                'sentry_options' => []
-            ]
-        ]*/
-    ],
-    'server_error_production' => 'An error occurred.',
-];
+           'formatters'              => [
+                                         SymfonyException\UnprocessableEntityHttpException::class => Formatters\UnprocessableEntityHttpExceptionFormatter::class,
+                                         SymfonyException\HttpException::class                    => Formatters\HttpExceptionFormatter::class,
+                                         \Illuminate\Validation\ValidationException::class        => Formatters\ValidationExceptionFormatter::class,
+                                         PDOException::class                                      => Formatters\PDOExceptionFormatter::class,
+                                         \Illuminate\Auth\AuthenticationException::class          => Formatters\AuthExceptionFormatter::class,
+                                         Exception::class                                         => Formatters\ExceptionFormatter::class,
+                                        ],
+           'response_factory'        => ResponseFactory::class,
+           'reporters'               => [],
+           'server_error_production' => 'An error occurred.',
+          ];
+// phpcs:enable Generic.Files.LineLength.MaxExceeded,Generic.Files.LineLength.TooLong
+return $config;
+/*'reporters' => [
+    'sentry' => [
+        'class'  => \Optimus\Heimdal\Reporters\SentryReporter::class,
+        'config' => [
+            'dsn' => '',
+            // For extra options see https://docs.sentry.io/clients/php/config/
+            // php version and environment are automatically added.
+            'sentry_options' => []
+        ]
+    ]
+]*/
