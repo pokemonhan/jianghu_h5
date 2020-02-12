32fa318a131d86c93b7dbea1df28545bdfb7aafb
alvin <953909858@qq.com>
2020-02-12 17:32:07 +0800

    添加洗码设置规则表

---
+++ database/migrations/2020_02_12_174231_create_users_commission_configs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateUsersCommissionConfigsTable
+ */
+class CreateUsersCommissionConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(
+            'users_commission_configs',
+            static function (Blueprint $table): void {
+                $table->bigIncrements('id');
+                $table->string('platform_sign', 32)->nullable()->default(null)->comment('平台标识');
+                $table->integer('game_type_id')->default(0)->comment('游戏类型ID');
+                $table->integer('game_vendor_id')->default(0)->comment('游戏厂商ID');
+                $table->decimal('bet', 18, 4)->nullable()->default(null)->comment('打码量');
+                $table->timestamps();
+            },
+        );
+        DB::statement("ALTER TABLE `users_commission_configs` comment '佣金返点规则'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_commission_configs');
+    }
+}
---
+++ database/migrations/2020_02_12_174346_create_users_commission_config_details_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateUsersCommissionConfigDetailsTable
+ */
+class CreateUsersCommissionConfigDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(
+            'users_commission_config_details',
+            static function (Blueprint $table): void {
+                $table->bigIncrements('id');
+                $table->integer('config_id')->default(0)->comment('所属洗码配置ID');
+                $table->integer('grade_id')->default(0)->comment('用户等级ID');
+                $table->decimal('percent', 5, 2)->nullable()->default(null)->comment('洗码比例');
+                $table->decimal('grade_exp_max', 18, 4)->nullable()->default(null)->comment('该等级最大经验值');
+                $table->timestamps();
+            },
+        );
+        DB::statement("ALTER TABLE `users_commission_config_details` comment '佣金返点规则详情'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_commission_config_details');
+    }
+}
