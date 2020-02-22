b70ab3c40c53f835e88d87a294bd608f2052c14f
Harris <harrisdt15f@gmail.com>
2020-02-22 17:17:58 +0800

    优化代码规范 文件: pagination.php

--- resources/lang/zh-CN/pagination.php
+++ resources/lang/zh-CN/pagination.php
@@ -1,6 +1,6 @@
 <?php
 
-return [
+$config = [
     /*
     |--------------------------------------------------------------------------
     | Pagination Language Lines
@@ -12,6 +12,7 @@ return [
     |
     */
 
-    'previous' => '&laquo; 上一页',
-    'next'     => '下一页 &raquo;',
-];
+           'previous' => '&laquo; 上一页',
+           'next'     => '下一页 &raquo;',
+          ];
+return $config;
