0aae96a2c7f1cd6d14f33f170b1441dbecde741e
Harris <harrisdt15f@gmail.com>
2020-02-22 17:16:26 +0800

    优化代码规范 文件: default-info.php

--- resources/lang/zh-CN/codes-and-messages/default-info.php
+++ resources/lang/zh-CN/codes-and-messages/default-info.php
@@ -1,11 +1,14 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * author: Harris
  * Date: 5/7/2019
  * Time: 9:47 PM
  */
-return [
-    '200' => '执行成功',
-    '404' => '出错了',
-];
+
+$config = [
+           '200' => '执行成功',
+           '404' => '出错了',
+          ];
+return $config;
