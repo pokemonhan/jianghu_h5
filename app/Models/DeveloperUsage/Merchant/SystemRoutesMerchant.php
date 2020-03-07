a23ed408438d7978659cd4f5ce948d18fc38d0be
alvin <953909858@qq.com>
2020-03-07 17:34:42 +0800

    总后台-路由列表-添加路由是否有效字段

--- app/Models/DeveloperUsage/Merchant/SystemRoutesMerchant.php
+++ app/Models/DeveloperUsage/Merchant/SystemRoutesMerchant.php
@@ -5,6 +5,7 @@ namespace App\Models\DeveloperUsage\Merchant;
 use App\Models\BaseModel;
 use App\Models\DeveloperUsage\Menu\MerchantSystemMenu;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Route;
 
 /**
  * Class for system routes merchant.
@@ -20,6 +21,14 @@ class SystemRoutesMerchant extends BaseModel
     /**
      * @var array
      */
+    protected $appends = [
+                          'real_route',
+                          'route_to_update',
+                         ];
+
+    /**
+     * @var array
+     */
     public static $fieldDefinition = [
                                       'id'            => '路由ID',
                                       'menu_group_id' => '所属菜单ID',
@@ -29,6 +38,29 @@ class SystemRoutesMerchant extends BaseModel
                                       'method'        => '方法',
                                       'is_open'       => '开放状态',
                                      ];
+
+    /**
+     * @return string
+     */
+    public function getRealRouteAttribute(): string
+    {
+        if (!Route::has($this->route_name)) {
+            return '';
+        }
+        $routeName = route($this->route_name, [], false);
+        return $routeName;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getRouteToUpdateAttribute(): int
+    {
+        if (!Route::has($this->route_name)) {
+            return 1;
+        }
+        return 0;
+    }
 
     /**
      * @return BelongsTo
