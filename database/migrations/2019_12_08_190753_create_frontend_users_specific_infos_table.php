868ef1c70d9d531a0f6adc74a53333a262318a06
Harris <harrisdt15f@gmail.com>
2020-03-10 17:08:46 +0800

    ✨ 添加游戏已创建用户状态 避免用户重新创建用户

--- database/migrations/2019_12_08_190753_create_frontend_users_specific_infos_table.php
+++ database/migrations/2019_12_08_190753_create_frontend_users_specific_infos_table.php
@@ -34,8 +34,8 @@ class CreateFrontendUsersSpecificInfosTable extends Migration
                     ->comment('注册类型：0.普通注册1.人工开户注册2.链接开户注册3.扫码开户注册');
                 $table->integer('total_members')->nullable()->default('0')->comment('用户发展客户总数');
                 $table->string('domain')->nullable()->default(null)->comment('所属域名');
-
-                $table->nullableTimestamps();
+                $table->text('g_active')->nullable()->comment('游戏已被注册判断字段 绑定的是厂商表的sign值为1,0,默认1');
+                $table->timestamps();
             },
         );
         DB::statement("ALTER TABLE `frontend_users_specific_infos` comment '用户信息拓展表'");
--- database/seeds/FrontendUsersSpecificInfoSeeder.php
+++ database/seeds/FrontendUsersSpecificInfoSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User\FrontendUsersSpecificInfo;
 use Illuminate\Database\Seeder;
 
 /**
@@ -19,42 +18,45 @@ class FrontendUsersSpecificInfoSeeder extends Seeder
             [
              [
               'id'            => 1,
+              'user_id'       => 1,
+              'level'         => 2,
               'nickname'      => 'harriszhongdai',
+              'experience'    => 2500,
               'avatar'        => 1,
               'email'         => '1823444@qq.com',
               'zip_code'      => '233333',
               'address'       => '朝歌',
               'register_type' => 0,
               'total_members' => 20,
-              'user_id'       => 1,
-              'level'         => 2,
-              'experience'    => 2500,
+              'g_active'      => '{"ky":1,"vr":0}',
              ],
              [
               'id'            => 2,
+              'user_id'       => 2,
+              'level'         => 1,
               'nickname'      => '亚索Suo',
+              'experience'    => 1500,
               'avatar'        => 1,
               'email'         => 'sunshine@gmail.com',
               'zip_code'      => '1600',
               'address'       => 'State of California',
               'register_type' => 0,
               'total_members' => 15,
-              'user_id'       => 2,
-              'level'         => 1,
-              'experience'    => 1500,
+              'g_active'      => null,
              ],
              [
               'id'            => 3,
+              'user_id'       => 3,
+              'level'         => 0,
               'nickname'      => '李青Lee',
+              'experience'    => 500,
               'avatar'        => 1,
               'email'         => 'test@qq.com',
               'zip_code'      => '233333',
               'address'       => 'test city',
               'register_type' => 0,
               'total_members' => 15,
-              'user_id'       => 3,
-              'level'         => 0,
-              'experience'    => 500,
+              'g_active'      => null,
              ],
             ],
         );
